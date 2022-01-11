104fb6f1d11e9491e52b335c91dddee763b29ec1
Tokmachev.Dmitry <Tokmachev.Dmitry@TOKMACHEV-NB-WX>
2022-01-11 14:02:35 +0300

    Add project files.

---
+++ XControls/XControlLabelEx.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.Blazor.Reporting.Designer.Components;
+using DevExpress.XtraReports.UI;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorReportDemos {
+    public class XControlLabelEx : XControlLabel {
+        [Parameter] public bool KeepTogether { get; set; } = false;
+        protected override XRLabel CreateControl() {
+            var label = base.CreateControl();
+            label.KeepTogether = KeepTogether;
+            return label;
+        }
+    }
+}
